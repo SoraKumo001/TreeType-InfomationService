34c96a1ed856d9fe34f12779236ce209368df486
SoraKumo <info@croud.jp>
2019-06-24 15:19:45 +0900

    ファイル構成の修正

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "tree-type-infomation-service",
+      script: "./dist/app/index.js",
+      watch: ["./dist/app"],
+      instances: 1,
+      exec_mode: "cluster_mode",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      merge_logs: true,
+      error_file: "./dist/log/error.log",
+      out_file: "./dist/log/access.log",
+      node_args: ["--no-warnings"],
+      env: {
+        NODE_OPTIONS: "--no-warnings"
+      }
+    }
+  ]
+};
